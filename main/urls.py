30b588f5dd80eb45ed5551692e9bedb32ec42746
Manish <techwork7236@gmail.com>
2020-11-01 08:05:28 +0530

    Hello

---
+++ main/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from main import views
+
+urlpatterns = [
+    path('', views.index, name="index"),
+    path('blog/', views.blog, name="blog"),
+    path('blog/post/<int:post_id>/', views.post, name="post"),
+    path('contact/', views.contact, name="contact"),
+]
